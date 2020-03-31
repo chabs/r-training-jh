f364b95eabf40f137bdf48b2928aa6d46a3ca579
Abby Walker <abby.walker@live.com>
2020-03-31 11:37:49 +0100

    initial commit. file to start swirl for exercises set in the class. refresher with git habits

---
+++ swirl_exercises.R
@@ -0,0 +1,12 @@
+# install swirl and check the version
+install.packages("swirl")
+packageVersion("swirl")
+
+# load the lib
+library(swirl)
+
+# install r programming course 
+install_from_swirl("R Programming")
+
+# start swirl, for week 1 look at exercises 1-7
+swirl()
